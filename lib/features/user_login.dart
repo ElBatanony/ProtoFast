fce747ed3b0140e74a8d14a6abf0c7c3c3a412be
ElBatanony <ahmedelbatanony@hotmail.com>
2021-09-21 17:14:49 +0300

    Add Login Button Display config to User Login

--- lib/features/user_login.dart
+++ lib/features/user_login.dart
@@ -1,9 +1,11 @@
 import 'package:protofast/configurations/bool_configuration.dart';
+import 'package:protofast/configurations/string_configuration.dart';
 import 'package:protofast/models/feature.dart';
 
 class UserLogin extends Feature {
   UserLogin()
       : super('User Login', [
-          BoolConfiguration('Can users login with their phone numbers?', false)
+          BoolConfiguration('Can users login with their phone numbers?', false),
+          StringConfiguration('What should the login button display?', 'Log In')
         ]);
 }
