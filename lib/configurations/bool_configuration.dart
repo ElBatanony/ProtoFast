8e83b8bc24e023f143cbaa1ee09aacebfc92f187
ElBatanony <ahmedelbatanony@hotmail.com>
2021-09-21 16:57:54 +0300

    Add BoolConfigurationWidget with Switch

--- lib/configurations/bool_configuration.dart
+++ lib/configurations/bool_configuration.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:protofast/models/configuration.dart';
 
 class BoolConfiguration extends Configuration {
@@ -13,6 +13,51 @@ class BoolConfiguration extends Configuration {
 
   @override
   Widget configurationWidget() {
-    return Row(children: [Text(question), Text(isEnabled.toString())]);
+    return BoolConfigurationWidget(boolConfig: this);
+  }
+
+  void setConfiguration(bool newConfig) {
+    isEnabled = newConfig;
+  }
+}
+
+class BoolConfigurationWidget extends StatefulWidget {
+  const BoolConfigurationWidget({Key? key, required this.boolConfig})
+      : super(key: key);
+
+  final BoolConfiguration boolConfig;
+
+  @override
+  _BoolConfigurationWidgetState createState() =>
+      _BoolConfigurationWidgetState();
+}
+
+class _BoolConfigurationWidgetState extends State<BoolConfigurationWidget> {
+  bool isEnabled = false;
+
+  @override
+  void initState() {
+    setState(() {
+      isEnabled = widget.boolConfig.isEnabled;
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [
+      Text(widget.boolConfig.question),
+      Switch(
+        value: isEnabled,
+        onChanged: (value) {
+          setState(() {
+            isEnabled = value;
+            widget.boolConfig.setConfiguration(isEnabled);
+          });
+        },
+        activeTrackColor: Colors.lightGreenAccent,
+        activeColor: Colors.green,
+      )
+    ]);
   }
 }
