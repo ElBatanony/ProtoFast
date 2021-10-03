79770ce223ee62b9c01be18a4a55ac6973ae22af
ElBatanony <ahmedelbatanony@hotmail.com>
2021-10-03 08:06:51 +0200

    Add navigation to new project screen

--- lib/screens/new_project_screen.dart
+++ lib/screens/new_project_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:protofast/data/projects_manager.dart';
+import 'package:protofast/models/project.dart';
+import 'package:protofast/screens/project_screen.dart';
 
 class NewProjectScreen extends StatefulWidget {
   const NewProjectScreen({Key? key}) : super(key: key);
@@ -10,22 +13,36 @@ class NewProjectScreen extends StatefulWidget {
 class _NewProjectScreenState extends State<NewProjectScreen> {
   TextEditingController projectNameController = TextEditingController();
 
-  void _createProject() {
-    print(projectNameController.text);
+  void _createProject() async {
+    Project newProject = Project(projectNameController.text, [], []);
+    await ProjectsManager.addProject(newProject);
+    Navigator.pop(context);
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => ProjectScreen(project: newProject)));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: const Text('Create a new project')),
-        body: Column(
-          children: [
-            TextField(
-                controller: projectNameController,
-                decoration: const InputDecoration(label: Text('Project Name'))),
-            ElevatedButton(
-                onPressed: _createProject, child: const Text('Create Project'))
-          ],
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                    controller: projectNameController,
+                    decoration:
+                        const InputDecoration(label: Text('Project Name'))),
+              ),
+              ElevatedButton(
+                  onPressed: _createProject,
+                  child: const Text('Create Project'))
+            ],
+          ),
         ));
   }
 }
