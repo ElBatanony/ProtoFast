ab8b9204d9d7546a816e2f696b4daa2fd3ce7172
mac <df_mokhtari@esi.dz>
2021-10-03 12:54:01 +0100

    User content + Feature Screen

--- lib/main.dart
+++ lib/main.dart
@@ -40,8 +40,7 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {});
     Navigator.push(
       context,
-      //MaterialPageRoute(builder: (context) => SrsScreen(project: myProject)),
-      MaterialPageRoute(builder: (context) => const UserContentScreen()),
+      MaterialPageRoute(builder: (context) => SrsScreen(project: myProject)),
     );
   }
 
